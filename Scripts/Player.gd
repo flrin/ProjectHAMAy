aa467b7f86e61d2b0dbae1733b78661d212872d1
florin <florea.m.florin@gmail.com>
2024-07-10 18:41:19 +0300

    Added detection area to the player and added pushback

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,16 +4,24 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const DODGE_ACCELERATION = 10
+const PUSHBACK_SPEED  = 500
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var dodge_accel = 1
 var collision_shape
+var hitbox_area
+var is_pushed = false
+var heart_ammount = 3
 
 func _ready():
 	collision_shape = $CollisionShape2D
+	hitbox_area = $HitboxArea
+	set_collision_mask_from_list([2,3,4,5], true)
 
 func _physics_process(delta):
+	#Process collisions
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -27,21 +35,47 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("ui_dodge"):
 			dodge_accel = DODGE_ACCELERATION
 			set_collision_mask_from_list([2,3,4,5], false)
+			hitbox_area.monitoring = false
 		else:
 			set_collision_mask_from_list([2,3,4,5], true)
+			hitbox_area.monitoring = true
 	else:
 		dodge_accel -= 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED * dodge_accel
+	if direction and is_pushed == false:
+		velocity.x = direction * SPEED * dodge_accel 
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta * 18.25)
+	if velocity.x == 0:
+		is_pushed = false
 	move_and_slide()
 
 func set_collision_mask_from_list(list_to_set, value): #value e ori true ori false
 	for i in list_to_set:
 		set_collision_mask_value(i, value)
+
+func take_damage(ammount, hit_position):
+	#Knock back the player
+	var pushback_direction = position - hit_position
+	pushback_direction = pushback_direction.normalized()
+	pushback_direction.x *=2
+	velocity = pushback_direction * PUSHBACK_SPEED
+	is_pushed = true
+
+	#Substract hearts
+	heart_ammount -= ammount
+	if heart_ammount <= 0:
+		game_over()
+
+func game_over():
+	queue_free()
+
+func _on_hitbox_area_entered(area):
+	take_damage(1, area.position)
+
+func _on_hitbox_body_entered(body):
+	take_damage(1, body.position)
+
